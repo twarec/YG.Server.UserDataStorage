1a3a0d11ccc9214c22f945a6a1ac1799bf8a72e4
twarec <guguev-jura@yandex.ru>
2024-06-26 12:05:01 +0300

    w

--- Extensions/ServiceExtensions.cs
+++ Extensions/ServiceExtensions.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
 using YG.Server.UserDataStorage.DataBase;
 using YG.Server.UserDataStorage.Services;
 using YG.Server.UserDataStorage.Services.Runtime;
@@ -25,6 +28,18 @@ public static class ServiceExtensions
             .AddScoped<IFieldService, FieldService>();
 
         return services;
+    }
+
+    public static SwaggerGenOptions AddUserDataStorage(this SwaggerGenOptions options)
+    {
+        options.SwaggerDoc(name: "UserDataStorage", new OpenApiInfo { Title = "UserDataStorage", Version = "UserDataStorage" });
+        return options;
+    }
+
+    public static SwaggerUIOptions AddUserDataStorage(this SwaggerUIOptions options)
+    {
+        options.SwaggerEndpoint(url: $"/swagger/UserDataStorage/swagger.json", name: "UserDataStorage");
+        return options;
     }
 
     public static IEndpointRouteBuilder UseUserDataStorage(this IEndpointRouteBuilder builder)
